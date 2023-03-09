df2384c086252a2a07b7f07f0190f148c6d40c9a
Frederico Leonardo <frederico@gomo.digital>
2023-03-09 22:01:29 +0000

    ..

--- src/main.js
+++ src/main.js
@@ -1,39 +1,83 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
-import library from './modules/library'
+import library from './modules/library';
 
-import './styles/style.css'
+import './styles/style.css';
 
-const currentYear = new Date().getFullYear()
-$('.copyright-year').text(currentYear)
+const currentYear = new Date().getFullYear();
+$('.copyright-year').text(currentYear);
 
 // Open external links in a new tab
-$('a[href^="http"]:not([href*="' + window.location.hostname + '"])').attr(
-  'target',
-  '_blank'
-)
+$('a[href^="http"]:not([href*="' + window.location.hostname + '"])').attr('target', '_blank');
 
 // Open accordion
-var $gridLine = $('.grid-line--diagonal-inverted')
+var $gridLine = $('.grid-line--diagonal-inverted');
 
 // Function to check the initial state of the accordion items and hide the grid line if necessary
 function checkAccordionState() {
   if ($('.accordion-item.is-open').length > 0) {
-    $gridLine.hide()
+    $gridLine.hide();
   } else {
-    $gridLine.show()
+    $gridLine.show();
   }
 }
 
 $('.accordion-trigger').on('click', function () {
-  $(this).parent('.accordion-item').toggleClass('is-open')
+  $(this).parent('.accordion-item').toggleClass('is-open');
   if ($('.accordion-item.is-open').length > 0) {
-    $gridLine.hide()
+    $gridLine.hide();
   } else {
-    $gridLine.show()
+    $gridLine.show();
   }
-})
+});
 
-checkAccordionState()
-library()
-console.log('Hello, world!')
+// Store the original location of the modal video element
+const originalLocation = $('.modal-video').parent();
+
+$('.play-button').on('click', function (e) {
+  e.preventDefault();
+  const targetId = $(this).attr('href');
+  const modalVideo = $(targetId);
+  modalVideo.detach();
+  $('body').append(modalVideo);
+  requestAnimationFrame(() => {
+    modalVideo.addClass('is-open');
+    $('body').css('overflow', 'hidden');
+  });
+});
+
+$('.modal-video_close').on('click', function (e) {
+  e.preventDefault();
+  const modalVideo = $(this).closest('.modal-video');
+  modalVideo.removeClass('is-open');
+  setTimeout(function () {
+    modalVideo.detach();
+    originalLocation.append(modalVideo);
+    $('body').css('overflow', 'auto');
+  }, 500); // Delay in milliseconds
+});
+
+const modals = $('.modal');
+modals.detach().appendTo('body');
+
+$(document).on('click', 'a[href^="#modal-"]', function (e) {
+  e.preventDefault();
+  const target = $(this).attr('href');
+  $(target).addClass('is-open');
+  $('body').css('overflow', 'hidden');
+  history.replaceState({}, document.title, window.location.href.split('#')[0]);
+});
+
+$('.modal_close').on('click', function (e) {
+  e.preventDefault();
+  const modal = $(this).closest('.modal');
+  modal.removeClass('is-open');
+  $('body').css('overflow', 'auto');
+});
+
+checkAccordionState();
+
+if (window.location.pathname.startsWith('/biblioteca') || window.location.pathname.startsWith('/artigo') || window.location.pathname.startsWith('/tema') || window.location.pathname.startsWith('/tag')) {
+  console.log('library');
+  library();
+}
--- src/modules/library.js
+++ src/modules/library.js
@@ -1,82 +1,81 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
 export default function library() {
-  const searchInput = $('#library-search_input')
-  const searchReset = $('#library-search_reset')
-  const searchSubmit = $('#library-search_submit')
-  const featuredArticles = $('.library-articles.is-featured')
-  const typeLinks = $('.library-types_item')
-  const typeClear = $('.library-types_clear')
+  const searchInput = $('#library-search_input');
+  const searchReset = $('#library-search_reset');
+  const searchSubmit = $('#library-search_submit');
+  const featuredArticles = $('.library-articles.is-featured');
+  const filterTrigger = $('.jetboost-filter-trigger');
+  const typesClear = $('.library-types_clear');
 
   // Set input type to "search" on page load
-  searchInput.attr('type', 'search')
-  searchReset.hide()
+  searchInput.attr('type', 'search');
+  searchReset.hide();
 
-  // Check if search input already has content or if there's a search term in the query string on page load
-  const searchParams = new URLSearchParams(window.location.search)
-  if (searchInput.val().length > 0 || searchParams.has('search')) {
-    hideFeaturedArticles()
-    searchReset.show()
-    searchSubmit.hide()
+  // Check if there are any query parameters on page load
+  const searchParams = new URLSearchParams(window.location.search);
+  if (searchInput.val().length > 0 || searchParams.toString().length > 0) {
+    hideFeaturedArticles();
+    searchReset.show();
+    searchSubmit.hide();
   } else {
-    showFeaturedArticles()
-    searchSubmit.show()
+    showFeaturedArticles();
+    searchSubmit.show();
   }
 
-  // Fade in search reset button when user starts typing
+  if (filterTrigger.hasClass('jetboost-filter-active')) {
+    hideFeaturedArticles();
+  } else {
+    showFeaturedArticles();
+  }
+
+  // Show featured articles when filter is clicked
+  filterTrigger.on('click', function () {
+    if ($(this).hasClass('jetboost-filter-active')) {
+      hideFeaturedArticles();
+    } else {
+      showFeaturedArticles();
+    }
+  });
+
+  // Show featured articles when clear button is clicked
+  typesClear.on('click', function () {
+    if (searchInput.val().length === 0) {
+      showFeaturedArticles();
+    }
+  });
+
+  // Fade in search reset button when typing
   searchInput.on('input', () => {
     if (searchInput.val().length > 0) {
-      hideFeaturedArticles()
-      searchReset.show()
-      searchSubmit.hide()
+      hideFeaturedArticles();
+      searchReset.show();
+      searchSubmit.hide();
     } else {
-      showFeaturedArticles()
-      searchReset.hide()
-      searchSubmit.show()
+      showFeaturedArticles();
+      searchReset.hide();
+      searchSubmit.show();
     }
-  })
+  });
 
   // Clear search input and fade out reset button when reset button is clicked
   searchReset.on('click', () => {
-    showFeaturedArticles()
-    searchReset.hide()
-    searchSubmit.show()
-  })
+    showFeaturedArticles();
+    searchReset.hide();
+    searchSubmit.show();
+  });
 
-  // Hide featured articles when type link is active
-  typeLinks.on('click', function (event) {
-    event.preventDefault()
-    const isOtherActive =
-      typeLinks.filter(function () {
-        return $(this).hasClass('jetboost-filter-active')
-      }).length > 0
-    if (
-      isOtherActive ||
-      typeLinks.filter('.jetboost-filter-active').length === 0
-    ) {
-      hideFeaturedArticles()
-    } else {
-      showFeaturedArticles()
-    }
-  })
-
-  // Show featured articles when type clear link is clicked
-  typeClear.on('click', () => {
-    if (searchInput.val().length === 0) {
-      showFeaturedArticles()
-    }
-    typeLinks.removeClass('jetboost-filter-active')
-  })
-
+  // Show featured articles
   function showFeaturedArticles() {
     if (featuredArticles.length > 0) {
-      featuredArticles.show()
+      featuredArticles.show();
     }
   }
 
+  // Hide featured articles
   function hideFeaturedArticles() {
     if (featuredArticles.length > 0) {
-      featuredArticles.hide()
+      featuredArticles.hide();
     }
   }
 }
